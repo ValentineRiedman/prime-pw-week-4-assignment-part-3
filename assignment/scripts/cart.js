5c88298dd41c665cbf3f4f80af3d6700f9339aa0
Valentine Riedman <underblueskysismile@gmail.com>
2022-04-04 17:28:26 -0500

    stretch goals added

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,9 +5,13 @@ let basket =[];
 const maxItems=5;
 console.log('Basket is ', basket);
 function  addItem( item ){
-    basket.push( item )
-       return true;  
-       
+    if(isFull() === false){
+      basket.push( item );
+        return true;
+    }
+    if(isFull() === true ){
+        return false;
+    }       
     
 }//end of addItem 
         
@@ -16,7 +20,8 @@ console.log('Adding lime (expect true)', addItem('lime'));
 console.log('Adding mango (expect true)', addItem('mango'));
 console.log('Adding orange (expect true)',addItem('orange'));
 console.log('Adding lemon (expect true)',addItem('lemon'));
-console.log('Adding banana (expect true)',addItem('banana'));
+console.log('Adding banana (expect false)',addItem('banana'));
+console.log('Adding pineapple (expect false)',addItem('pineapple'));
 console.log('Items in the basket are', basket);
 
 
@@ -49,3 +54,17 @@ function isFull(){
     }//end of isFull
 console.log('The basket is full?', isFull());
 
+function removeItem( item ){
+    // if(basket.indexOf === item ){
+    //     basket.slice( 0, 1, item );  
+    // } else{
+    //     return null;
+    // }
+    if( basket.indexOf(item)){
+    basket.splice(basket.indexOf( item ),1);
+    }else{
+        return null;
+    }
+}//end of removeItem
+console.log('No more limes', removeItem ('lime'));
+console.log(basket);
